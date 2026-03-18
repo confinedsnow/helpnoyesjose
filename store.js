590d193cfc99acd1cd87e887a6733574e6ace244
Confinedsnow <linuxluckisreal@gmail.com>
2026-03-17 21:01:59 -0400

    Delete store.js

--- store.js
+++
@@ -1,9 +0,0 @@
-global._servers = global._servers || {};
-global._queue   = global._queue   || {};
-global._logs    = global._logs    || [];
-
-module.exports = {
-    servers: global._servers,
-    queue:   global._queue,
-    logs:    global._logs,
-};
